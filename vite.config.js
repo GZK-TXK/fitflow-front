fc1aec2f274ed2f3f47f96f7a2c06b6e06f0f754
Gaizka <chocarrovaz.gaizka@gmail.com>
2026-09-14 13:03:03 +0200

    Correccion de error en vite para despliegue en Vercel

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,4 @@ export default defineConfig({
   server: {
     port: 5173,
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
-  },
 })
